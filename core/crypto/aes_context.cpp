a8e1b48cbfdc05c235bbf507f514b8b8c6de53a2
faKeZeroyt170 <paquonick67@gmail.com>
2026-02-24 20:34:36 +0100

    Update aes_context.cpp

--- core/crypto/aes_context.cpp
+++ core/crypto/aes_context.cpp
@@ -2,8 +2,7 @@
 /*  aes_context.cpp                                                       */
 /**************************************************************************/
 /*                         This file is part of:                          */
-/*                             GODOT ENGINE                               */
-/*                        https://godotengine.org                         */
+/*                             Nexus Engine                        */
 /**************************************************************************/
 /* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
